bd5c7671eaba82f3811816b46b1f8eb88f397075
artem_kr <artemlugynets@icloud.com>
2021-09-29 10:56:16 -0700

    onix test ios

---
+++ testapp3/testapp3/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  testapp3
+//
+//  Created by Artem Lugynets on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var textfromurl: UILabel!
+    @IBAction func click(_ sender: UIButton) {
+    
+        let url = URL(string: "http://numbersapi.com/random/year")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) {data, response, error in
+        if (error != nil) {
+            print("Server error is", error ?? "unknown")
+            return
+        }
+            
+            self.textfromurl.text = String(data: data!, encoding: String.Encoding.utf8)
+    }.resume()
+    }
+    
+
+}
